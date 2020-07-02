bc917bdc899ed229d38ab9bb87ea70b7b0fffc6f
Romina Taras <romina.taras@gmail.com>
2020-07-02 10:20:42 -0400

    Se agrega ejercicio de sesion realizado en clases.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ejercicios-sesion
 Ejercicios en clases
+
+Recordatorio:
+En proyectos reales, solo se suben los .java, html, etc.
+
+No se suben los 
+.classpath .project .class etc.
---
+++ src/com/edutecno/ejercicios/EjemploSesion.java
@@ -0,0 +1,35 @@
+package com.edutecno.ejercicios;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.edutecno.ejercicios.model.EntradaConcierto;
+
+@WebServlet("/creaSesion")
+public class EjemploSesion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String usuario = request.getParameter("nombre");
+		EntradaConcierto entrada = new EntradaConcierto();
+		HttpSession sesion = request.getSession(true);
+		PrintWriter writer = response.getWriter();
+		entrada.setNombreArtista("Iron Maiden");
+		entrada.setFechaConcierto(new Date());
+		entrada.setUbicacion("Cancha");
+		entrada.setTitularEntrada(usuario);
+		sesion.setAttribute("datosCompra", entrada);
+		writer.println("<html><body>");
+		writer.println("<h1>Entradas para " + entrada.getNombreArtista() + "</h1>");
+		writer.println("<h1>A nombre de " + entrada.getTitularEntrada() + "</h1>");
+		writer.println("</body></html>");
+	}
+}
